b554ca382355d9d65ec4cd5fc592cb056e408acf
Sahil Verma <domainsahil007@gmail.com>
2023-01-27 21:39:57 +0530

    unwanted library removed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,10 @@
-// import { QueryClient, QueryClientProvider } from "react-query";
-// import { ReactQueryDevtools } from "react-query/devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-
-// const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <QueryClientProvider client={queryClient}> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-      {/* <ReactQueryDevtools /> */}
-    {/* </QueryClientProvider> */}
+    <App />
   </React.StrictMode>
 );
